ee9b628acf04642e3b5810650d2795e67d426f3a
Ricky Huget <117780303+rickyhuget@users.noreply.github.com>
2023-01-24 10:37:41 -0800

    fixed spacing

--- ClassName.h
+++ ClassName.h
@@ -7,7 +7,7 @@
 class ClassName : public AbstractName {
   string combine(string name) {
     stripFirstFour(name);
-	stripLeadingSpaces(name);
+    stripLeadingSpaces(name);
     makeFirstLetterUppercase(name);
     deleteSpacesAndMakeUppercase(name);
     return name;
